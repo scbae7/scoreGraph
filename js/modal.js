b1e7f9b3a2853f21e18085a1b88880f19789878f
scbae <scbae27@gmail.com>
2025-05-07 15:43:18 +0900

    modal esc

--- js/modal.js
+++ js/modal.js
@@ -7,3 +7,12 @@ document.getElementById("closeAlert").onclick = function () {
   document.getElementById("alertModule").style.display = "none";
   document.getElementById("popUp").style.display = "none";
 }
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    // closeAlert 버튼 클릭한 것처럼 동작
+    const closeBtn = document.getElementById("closeAlert");
+    if (closeBtn) {
+      closeBtn.click();
+    }
+  }
+});
